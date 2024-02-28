9cf851c13cfe5f6275c4409edb0cff3dfba326ba
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-02-27 22:04:19 -0500

    [Enhancement] Improve !fn shop command

--- fortnite.py
+++ fortnite.py
@@ -1,13 +1,21 @@
 import requests
 import json
 import discord   # pip install discord
-from datetime import date
+from datetime import date, datetime, timedelta
 
 
 def get_shop_items():
   # Format todays shop URL
-  today_date = str(date.today())
-  formatted_string = today_date.replace("-", "_") + "_en.png"
+  current_time = datetime.now()
+
+  # Check if its after 7pm EST
+  #   At 7pm the Shop updates to the next day (update day accordingly)
+  if int(str(current_time).split()[1][:2]) >= 19:
+    shop_date = date.today() + timedelta(days=1)  
+  else:
+    shop_date = str(date.today())
+
+  formatted_string = str(shop_date).replace("-", "_") + "_en.png"
   url = 'https://shop.easyfnstats.com/' + formatted_string
   print(url)
 
