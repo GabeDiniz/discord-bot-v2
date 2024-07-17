7f27b09efc6d5f0cf57f622b6879a92b35602844
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-07-16 22:01:27 -0400

    [update] implement random gif command

--- functions/get_gif.py
+++ functions/get_gif.py
@@ -1,4 +1,17 @@
 import requests
 
-def random_gif():
-  pass
+# Fetch Credentials from local .env variables 
+from decouple import config
+GIPHY_API_KEY = config('GIPHY_API_KEY')
+
+# ["data"]["embed_url"]
+
+def random_gif(ctx):
+  url = f"https://api.giphy.com/v1/gifs/random?api_key={GIPHY_API_KEY}&tag=&rating=g"
+  response = requests.get(url)
+  if response.status_code == 200:
+      data = response.json()
+      data = data["data"]["embed_url"]
+      if data:
+        return data
+  return None
