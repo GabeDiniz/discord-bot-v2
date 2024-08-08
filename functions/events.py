0f176098d83b1d96c2a01d99c56cf3c0cfb794ad
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-08-07 22:18:41 -0400

    [update] remove deprecated code

--- functions/events.py
+++ functions/events.py
@@ -20,30 +20,3 @@ async def create_event(ctx: discord.Interaction, date: str, time: str, descripti
     privacy_level=discord.PrivacyLevel.guild_only  # Set the privacy level to guild only
   )
   await ctx.response.send_message(f"Event created! Check it out [here]({event.url})")
-
-
-# Handle reactions for RSVP
-# Catch reactions and update RSVP list
-# async def on_reaction_add(reaction, user):
-#   # Check if the reaction is on an event message and is a checkmark
-#   if reaction.emoji == "✅" and not user.bot:
-#     message = reaction.message
-#     if message.embeds and "New Event Created" in message.embeds[0].title:
-#       # Pull embed info
-#       embed = message.embeds[0]
-#       description = embed.description
-#       date = embed.fields[0].value
-#       time = embed.fields[1].value
-
-#       # Update the embed to show the user's RSVP
-#       if "RSVPs" not in embed.fields:
-#         embed.add_field(name="RSVPs", value=user.display_name, inline=False)
-#       else:
-#         rsvp_field_index = 2
-#         current_rsvps = embed.fields[rsvp_field_index].value
-#         # Make sure the user is not already RSVP'd
-#         if user.display_name not in current_rsvps:
-#           current_rsvps += f", {user.display_name}"
-#           embed.set_field_at(rsvp_field_index, name="RSVPs", value=current_rsvps, inline=False)
-
-#       await message.edit(embed=embed)
