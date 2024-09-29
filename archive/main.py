53d9e29c4136e41cf5b358d8fd53c8118b9eeec8
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-09-28 21:22:23 -0400

    [create] archive folder for deprecated commands

---
+++ archive/main.py
@@ -0,0 +1,13 @@
+# This file holds functions main.py functions (commands) that are deprecated 
+#   or no longer useful.
+
+# ========================================
+# SLASH COMMANDS
+# ========================================
+
+
+# #####################
+# [DEPRECATED]
+# @bot.tree.command(name="poll", description="Create a poll", guild=None)
+# async def poll_slash_command(interaction: discord.Interaction, question: str):
+#   await polls.create_poll(interaction, question)
---
+++ archive/polls.py
@@ -0,0 +1,33 @@
+import discord   # pip install discord
+
+# ========================================
+# DEPRECATED
+# ========================================
+async def create_poll(ctx, message):
+  # CATCH EMPTY POLL
+  if message.strip() == "":
+    embed = discord.Embed(
+      title=":bangbang: E R R O R",
+      description="Your poll message is empty. Please add your poll question after !poll",
+      color=discord.Color.red()
+    )
+    await message.channel.send(embed=embed)
+    return
+
+  embed = discord.Embed(
+    title=f"Poll: {message}",
+    description="React to vote",
+    color=discord.Color.green()
+  )
+
+  # If ctx is an interaction, respond to it
+  # This sends the confirmation message to the user
+  if isinstance(ctx, discord.Interaction):
+    await ctx.response.send_message("Poll created!", ephemeral=True)
+
+  # print(f"[ LOG ] creating poll with message: {message}\n")
+  await ctx.channel.send(embed=embed)
+  # for emoji in emojis:
+  #   await msg.add_reaction(emoji)
+  
+  
