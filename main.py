168038b411dfe89e20631640960c9a8d9e840870
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-10-13 13:53:35 -0400

    [fix] quotation consistency in main.py

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ import functions.nfl_sleeper as nfl_sleeper
 
 # Fetch Credentials from local .env variables 
 # Constants
-BOT_KEY = config('BOT_KEY')
-# OPENAI_API_KEY = config('OPENAI_API_KEY')
-knowledge: dict = responses.load_knowledge('./knowledge/knowledge2.json')
+BOT_KEY = config("BOT_KEY")
+# OPENAI_API_KEY = config("OPENAI_API_KEY")
+knowledge: dict = responses.load_knowledge("./knowledge/knowledge2.json")
 
 # Bot Constants
 intents = Intents.default()
@@ -39,7 +39,7 @@ bot = commands.Bot(command_prefix="!", intents=intents, help_command=None) # Ini
 # Start bot
 @bot.event
 async def on_ready():
-  '''Runs when the discord bot has connected'''
+  """Runs when the discord bot has connected"""
   load_wishlist()
   await steam_sale.start()
   await bot.tree.sync()
@@ -49,31 +49,31 @@ async def on_ready():
 # FUNCTIONS
 # ========================================
 def save_wishlist():
-  '''Saves the server wishlist when !addwishlist or !removewishlist is called'''
+  """Saves the server wishlist when !addwishlist or !removewishlist is called"""
   global server_wishlists
-  with open('wishlist.json', 'w') as f:
+  with open("wishlist.json", "w") as f:
     json.dump(server_wishlists, f)
   print("[ SAVED ] Wishlist saved successfully")
 
 def save_default_channel():
-  '''Saves the default channel'''
+  """Saves the default channel"""
   global default_wishlist_channel
-  with open('default_channel.json', 'w') as f:
+  with open("default_channel.json", "w") as f:
     json.dump(default_wishlist_channel, f)
     print("[ SAVED ] Default wishlist channel saved successfully")
 
 def load_wishlist():
-  '''Load wishlist if existing, create a new one if none available'''
+  """Load wishlist if existing, create a new one if none available"""
   global server_wishlists
   global default_wishlist_channel
   try:
     print("[ LOG ] Loading Server Wishlist...")
-    with open('wishlist.json', 'r') as f:
+    with open("wishlist.json", "r") as f:
       server_wishlists = json.load(f)
     print("[ LOG ] LOADED existing Wishlist")
 
     print("[ LOG ] Loading Default Wishlist channel data...")
-    with open('default_channel.json', 'r') as f:
+    with open("default_channel.json", "r") as f:
       default_wishlist_channel = json.load(f)
     print("[ LOG ] LOADED Default Wishlist channel")
   except FileNotFoundError:
@@ -84,81 +84,81 @@ def load_wishlist():
 
 @tasks.loop(hours=12)
 async def steam_sale():
-  '''Check each server wishlist for a sale and notify server'''
+  """Check each server wishlist for a sale and notify server"""
   print("[ LOG ] Loop-task: checking for server steam wishlist sale")
   await steam.check_sale(bot, server_wishlists, default_wishlist_channel)
 
 @steam_sale.before_loop
 async def before_check_sales():
-  '''Ensure the bot is ready before starting the steam_sale loop'''
+  """Ensure the bot is ready before starting the steam_sale loop"""
   await bot.wait_until_ready()
 
 # ========================================
 # ! COMMANDS
 # ========================================
 # Simple example command
-@bot.command(name='hello')
+@bot.command(name="hello")
 async def hello_command(ctx):
-  await ctx.send(f'Hello, {ctx.author.name}!')
+  await ctx.send(f"Hello, {ctx.author.name}!")
 
 # #####################
-@bot.command(name='help')
+@bot.command(name="help")
 async def help_command(ctx):
   embed = discord.Embed(
-    # title='Cpt. Bot Commands',
+    # title="Cpt. Bot Commands",
     color=discord.Color.red()
   )
   # Add fields to the embed (optional)
   embed.set_author(name="King Bob's Commands", url="https://github.com/GabeDiniz", icon_url="https://imgur.com/nH32raP.png")
   embed.set_thumbnail(url="https://i.imgur.com/nH32raP.png")
-  embed.add_field(name='See Music Commands', value='`!help-music`', inline=False)
-  embed.add_field(name='Search for a Game', value='`!steamgame <name>`', inline=True)
-  embed.add_field(name='Steam Server Wishlist', value='`!wishlist` `!addwishlist <game-name>` `!removewishlist <game-name>`', inline=True)
-  embed.add_field(name='CS2 Stats', value='`!cs2 <name>`', inline=True)
-  embed.add_field(name='Today\'s Fortnite Shop', value='`!fn-shop`', inline=True)
-  embed.add_field(name='Create a QR', value='Color-code should be in Hex format (#FFFFFF)\n`!qr <link> <optional: fg-color> <optional: bg-color>`', inline=False)
-  embed.add_field(name='Random GIF', value='`!gif`', inline=True)
-  embed.add_field(name='Create events!', value='`/event`', inline=True)
+  embed.add_field(name="See Music Commands", value="`!help-music`", inline=False)
+  embed.add_field(name="Search for a Game", value="`!steamgame <name>`", inline=True)
+  embed.add_field(name="Steam Server Wishlist", value="`!wishlist` `!addwishlist <game-name>` `!removewishlist <game-name>`", inline=True)
+  embed.add_field(name="CS2 Stats", value="`!cs2 <name>`", inline=True)
+  embed.add_field(name="Today\"s Fortnite Shop", value="`!fn-shop`", inline=True)
+  embed.add_field(name="Create a QR", value="Color-code should be in Hex format (#FFFFFF)\n`!qr <link> <optional: fg-color> <optional: bg-color>`", inline=False)
+  embed.add_field(name="Random GIF", value="`!gif`", inline=True)
+  embed.add_field(name="Create events!", value="`/event`", inline=True)
         
   # Send the embed message to the same channel where the command was issued
   await ctx.channel.send(embed=embed)
 
 # #####################
-@bot.command(name='help-music')
+@bot.command(name="help-music")
 async def help_command(ctx):
   embed = discord.Embed(
     color=discord.Color.red()
   )
   embed.set_author(name="Music Commands", url="https://github.com/GabeDiniz", icon_url="https://imgur.com/nH32raP.png")
   embed.set_thumbnail(url="https://i.imgur.com/nH32raP.png")
-  embed.add_field(name='Play or queue a song', value='`!play <song-query>`', inline=False)
-  embed.add_field(name='Skip current song', value='`!skip`', inline=False)
-  embed.add_field(name='Kick bot from VC', value='`!leave`', inline=False)
+  embed.add_field(name="Play or queue a song", value="`!play <song-query>`", inline=False)
+  embed.add_field(name="Skip current song", value="`!skip`", inline=False)
+  embed.add_field(name="Kick bot from VC", value="`!leave`", inline=False)
 
   # Send the embed message to the same channel where the command was issued
   await ctx.channel.send(embed=embed)
 
 # #####################
-@bot.command(name='cs2')
+@bot.command(name="cs2")
 async def steam_command(ctx, *, message: str):
   embed = steam.get_user_stats(ctx, message)
   await ctx.channel.send(embed=embed)
   
 # #####################
-@bot.command(name='fn-shop')
+@bot.command(name="fn-shop")
 async def play_command(ctx):
   embed = fortnite.get_shop_items()
   await ctx.channel.send(embed=embed)
 
 # #####################
-@bot.command(name='qr')
+@bot.command(name="qr")
 async def play_command(ctx, *, message: str):
   qr_code_file = qr.generate(message)   # Returns QR file
   if qr_code_file:
-    with open(qr_code_file, 'rb') as f:   # Open in Binary format
+    with open(qr_code_file, "rb") as f:   # Open in Binary format
       image = discord.File(f)
       embed = discord.Embed(
-        title='Created New QR',
+        title="Created New QR",
         color=discord.Color.red()
       )
       embed.set_image(url=f"attachment://{qr_code_file}")
@@ -168,7 +168,7 @@ async def play_command(ctx, *, message: str):
   os.remove("qr.png")
 
 # #####################
-@bot.command(name='gif')
+@bot.command(name="gif")
 async def play_command(ctx):
   gif = gifs.random_gif()
   await ctx.channel.send(gif)
@@ -181,19 +181,19 @@ async def steamgame(ctx, *, game_name: str):
   if isinstance(game_details, str):
     await ctx.send(game_details)
   else:
-    formatted_game_id = steam.reformat_game_id(game_details['name'])
+    formatted_game_id = steam.reformat_game_id(game_details["name"])
     print(f"[ LOG ] Formatted SteamGame ID: {formatted_game_id}")
-    game_url = f"https://store.steampowered.com/app/{game_details['steam_appid']}/{formatted_game_id}/"
-    discount = game_details.get('price_overview', {}).get('discount_percent', 0)
+    game_url = f"https://store.steampowered.com/app/{game_details["steam_appid"]}/{formatted_game_id}/"
+    discount = game_details.get("price_overview", {}).get("discount_percent", 0)
     embed = discord.Embed(
-      title=game_details['name'] + f' (ON SALE - {discount}% OFF)' if discount != 0 else game_details['name'], 
-      description=game_details.get('short_description', 'No description available.'), 
+      title=game_details["name"] + f" (ON SALE - {discount}% OFF)" if discount != 0 else game_details["name"], 
+      description=game_details.get("short_description", "No description available."), 
       color=discord.Color.red() if discount != 0 else discord.Color.purple())
-    embed.add_field(name="Price", value=f"{game_details['price_overview']['final_formatted']}" if 'price_overview' in game_details else "Free or Price Not Available")
-    embed.add_field(name="Genres", value=', '.join([genre['description'] for genre in game_details['genres']]))
-    embed.add_field(name="App ID", value=f"{game_details['steam_appid']}")
+    embed.add_field(name="Price", value=f"{game_details["price_overview"]["final_formatted"]}" if "price_overview" in game_details else "Free or Price Not Available")
+    embed.add_field(name="Genres", value=", ".join([genre["description"] for genre in game_details["genres"]]))
+    embed.add_field(name="App ID", value=f"{game_details["steam_appid"]}")
     embed.add_field(name="Steam Store", value=f"{game_url}", inline=False)
-    embed.set_thumbnail(url=game_details['header_image'])
+    embed.set_thumbnail(url=game_details["header_image"])
     await ctx.send(embed=embed)
 
 # #####################
@@ -221,7 +221,7 @@ async def nfl_league_info(ctx):
 
 # #####################
 # WIP: NOT FUNCTIONING AS IT COSTS MONEY
-# @bot.command(name='gpt')
+# @bot.command(name="gpt")
 # async def chatgpt(ctx, *, query: str):
 #   await ctx.typing()  # Show typing indicator while processing
 
@@ -252,18 +252,18 @@ async def create_event(interaction: discord.Interaction, date: str, time: str, d
 # ========================================
 # PLAY MUSIC
 # ========================================
-@bot.command(name='play')
+@bot.command(name="play")
 async def play_command(ctx, *, message: str):
   print(ctx)
   embed = await playmusic.play_music(ctx, message, bot)
   await ctx.channel.send(embed=embed)
 
-@bot.command(name='leave')
+@bot.command(name="leave")
 async def leave_command(ctx):
   embed = await playmusic.leave_channel(ctx)
   await ctx.channel.send(embed=embed)
 
-@bot.command(name='skip')
+@bot.command(name="skip")
 async def skip_command(ctx):
   await playmusic.skip_song(ctx, bot) 
 
@@ -281,7 +281,7 @@ async def on_message(ctx):
   #   response: str = "msg here"
 
   if ctx.content and ctx.author != bot.user:
-    print(f'[ {ctx.channel} ] {ctx.author}: "{ctx.content}"')
+    print(f"[ {ctx.channel} ] {ctx.author}: '{ctx.content}'")
     response = responses.get_response(ctx.content, knowledge=knowledge)
     if response:
       await ctx.channel.send(response)
