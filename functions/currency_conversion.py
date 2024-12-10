0c51a9ebe05f99b2b8bc6321244b4c6bc16b17b3
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-12-09 19:12:52 -0500

    [add] description to conversion functions

--- functions/currency_conversion.py
+++ functions/currency_conversion.py
@@ -7,6 +7,21 @@ from decouple import config
 EXCHANGE_RATE_API_KEY = config('EXCHANGE_RATE_API_KEY')
 
 def get_rates(base_currency):
+  """
+  Search for the conversion rate off of the base currency using the Exchange Rate API.
+
+  Parameters
+  ----------
+  base_currency: str
+    The exchange rate token that the results will be based off of. 
+
+  Returns
+  -------
+  dic:
+    The conversion rate results based on the base currency.
+  str:
+    If no match is found that meets the cutoff criteria.
+  """
   api_url = f"https://v6.exchangerate-api.com/v6/{EXCHANGE_RATE_API_KEY}/latest/{base_currency}"
   response = requests.get(api_url)
 
@@ -18,6 +33,25 @@ def get_rates(base_currency):
     return f"{base_currency} not found (status code: {response.status_code})"
 
 async def convert_currency(ctx, amount, from_currency, to_currency):
+  """
+  Description
+
+  Parameters
+  ----------
+  ctx: discord.Bot
+    The Discord bot instance used to interact with channels and send messages.
+  amount: float
+    The amount to be converted.
+  from_currency: str
+    The currency that the amount is based off of.
+  to_currency: str
+    The currency that the amount will be converted to.
+
+  Returns
+  -------
+  None
+    Sends a Discord embed message of the converted amount from from_currency to to_currency.
+  """
   from_currency, to_currency = from_currency.upper(), to_currency.upper()
   rates = get_rates(from_currency)
 
