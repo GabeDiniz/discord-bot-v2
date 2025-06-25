e338294863ffebcbacd03520bcfd91793469d33d
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-06-24 21:14:08 -0400

    [add] ai prompt command to help

--- main.py
+++ main.py
@@ -121,6 +121,7 @@ async def help_command(ctx):
   embed.add_field(name='See Music Commands', value='`!help-music`', inline=False)
   embed.add_field(name='Search for a Game', value='`!steamgame <name>`', inline=True)
   embed.add_field(name='Steam Server Wishlist', value='`!wishlist` `!addwishlist <game-name>` `!removewishlist <game-name>`', inline=False)
+  embed.add_field(name='AI Prompt!', value='`!p <prompt>`', inline=True)
   embed.add_field(name='CS2 Stats', value='`!cs2 <name>`', inline=True)
   embed.add_field(name='Today\'s Fortnite Shop', value='`!fn-shop`', inline=True)
   embed.add_field(name='Create a QR', value='Color-code should be in Hex format (#FFFFFF)\n`!qr <link> <optional: fg-color> <optional: bg-color>`', inline=False)
